7d8b3b00baf40f07fd02d7f658386b8d0bb7469a
BT_1N3_29 <sample@bths.edu>
2024-10-25 13:11:19 -0400

    blah blqagh

---
+++ src/Charlatte.java
@@ -0,0 +1,5 @@
+public class Charlatte {
+    public static void main(String[] args) {
+        System.out.println("i hate everyone");
+    }
+}
--- src/Runner.java
+++ src/Runner.java
@@ -2,5 +2,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("1 + 1 = " + (1 + 1));
+        System.out.println("hi again");
     }
 }

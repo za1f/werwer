7b3fe833404e1fb67dcf8983405508a17c066478
BT_1N3_29 <sample@bths.edu>
2024-10-25 13:43:51 -0400

    Revert "blah blqagh"
    This reverts commit 7d8b3b00baf40f07fd02d7f658386b8d0bb7469a.

--- src/Charlatte.java
+++
@@ -1,5 +0,0 @@
-public class Charlatte {
-    public static void main(String[] args) {
-        System.out.println("i hate everyone");
-    }
-}
--- src/Runner.java
+++ src/Runner.java
@@ -2,6 +2,5 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("Hi");
         System.out.println("1 + 1 = " + (1 + 1));
-        System.out.println("hi again");
     }
 }

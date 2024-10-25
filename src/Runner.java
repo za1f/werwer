db0ebc36f3be65f598487c5a35b3bba675232eb3
BT_1N3_29 <sample@bths.edu>
2024-10-25 13:08:31 -0400

    changed something

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,6 @@
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Hi");
-        System.out.println(1 + 1 + " is the answer to 1 + 1");
+        System.out.println("1 + 1 = " + (1 + 1));
     }
 }
